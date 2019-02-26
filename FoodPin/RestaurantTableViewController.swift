b6434d8b2bfe18d891168e2fa6f9c62f6b6832d7
ValPacino <val.froidefond@gmail.com>
2019-02-26 20:04:52 +0100

    Table Row Detection, Swipe for Actions, Activity Controller

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -55,6 +55,14 @@ class RestaurantTableViewController: UITableViewController {
         //On déclare un UIAlertController qui s'affichera lors de l'interaction avec la cellule
         let optionMenu = UIAlertController(title: nil, message: "What do you want to do ?", preferredStyle: .actionSheet)
         
+        //Layout iPad
+        if let popoverController = optionMenu.popoverPresentationController {
+            if let cell = tableView.cellForRow(at: indexPath) {
+                popoverController.sourceView = cell
+                popoverController.sourceRect = cell.bounds
+            }
+        }
+        
         //On déclare une action (en lien avec le UIAlertController)
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
@@ -90,18 +98,97 @@ class RestaurantTableViewController: UITableViewController {
         })
         
         optionMenu.addAction(checkInAction)
+        
         present(optionMenu, animated: true, completion: nil)
         
         //Ne pas afficher le gris quand on intéragit avec la cellule
         tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    }
+    
+    // si l'utilisateur swipe vers la gauche, des boutons s'affichent sur la droite de la cellule
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
-        //Layout iPad
-        if let popoverController = optionMenu.popoverPresentationController {
-            if let cell = tableView.cellForRow(at: indexPath) {
-                popoverController.sourceView = cell
-                popoverController.sourceRect = cell.bounds
-            }
+        //Un bouton delete s'affiche sur la droite, il fera les actions demandés
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action,sourceView,completionHandler) in
+            self.restaurantNames.remove(at: indexPath.row)
+            self.restaurantLocations.remove(at: indexPath.row)
+            self.restaurantTypes.remove(at: indexPath.row)
+            self.restaurantIsVisited.remove(at: indexPath.row)
+            
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            //Masqué le handler par la suite
+            completionHandler(true)
         }
+        
+        
+        let shareAction = UIContextualAction(style: .normal, title: "Share") { (action,sourceView,completionHandler) in
+            let defaultText = "Just checking at " + self.restaurantNames[indexPath.row]
+            
+            //Un UIActivityController permet d'afficher un menu pour paratager le contenu souhaité
+            let activityController : UIActivityViewController
+            
+            if let imageToShare = UIImage(named: self.restaurantNames[indexPath.row]) {
+                activityController = UIActivityViewController(activityItems: [defaultText,imageToShare], applicationActivities: nil)
+            } else {
+                activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+            }
+            
+            //Layout iPad
+            if let popoverController = activityController.popoverPresentationController {
+                if let cell = tableView.cellForRow(at: indexPath) {
+                    popoverController.sourceView = cell
+                    popoverController.sourceRect = cell.bounds
+                }
+            }
+            
+            self.present(activityController, animated: true, completion: nil)
+            
+            completionHandler(true)
+        }
+        
+        deleteAction.backgroundColor = UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 60.0/255.0, alpha: 1.0)
+        deleteAction.image = UIImage(named: "delete")
+        
+        shareAction.backgroundColor = UIColor(red: 254.0/255.0, green: 149.0/255.0, blue: 38.0/255.0, alpha: 1.0)
+        shareAction.image = UIImage(named: "share")
+        
+        //On ajoute les SwipeActions dans un SwipeConfiguration que l'on retourne
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction,shareAction])
+        
+        
+        return swipeConfiguration
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let checkAction = UIContextualAction(style: .normal, title: "check") { (action,sourceView,completionHandler) in
+            let cell = tableView.cellForRow(at: indexPath)
+            
+            if self.restaurantIsVisited[indexPath.row] == false {
+                cell?.accessoryView = UIImageView(image: UIImage(named: "heart-tick"))
+                self.restaurantIsVisited[indexPath.row] = true
+            } else {
+                cell?.accessoryView = .none
+                self.restaurantIsVisited[indexPath.row] = false
+            }
+            
+            completionHandler(true)
+        }
+        
+        checkAction.backgroundColor = .green
+        if self.restaurantIsVisited[indexPath.row] == false {
+            checkAction.image = UIImage(named: "tick")
+        } else {
+            checkAction.image = UIImage(named: "undo")
+        }
+        
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [checkAction])
+        
+        return swipeConfiguration
     }
 
 }
