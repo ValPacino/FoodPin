1fa67662229299fba045970ed0a0d9e79c67dcd9
ValPacino <val.froidefond@gmail.com>
2019-03-05 22:49:29 +0100

    Adding a map interface to your app

---
+++ FoodPin/Controller/MapViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MapViewController.swift
+//  FoodPin
+//
+//  Created by Öcalan on 05/03/2019.
+//  Copyright © 2019 Froidefond Valentin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    var restaurant = Restaurant()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mapView.delegate = self
+        
+        //Get location
+        
+        let geoCoder = CLGeocoder()
+        
+        geoCoder.geocodeAddressString(restaurant.location, completionHandler : { placemarks, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            if let placemarks = placemarks {
+                //Get the first placemark
+                
+                let placemark = placemarks[0]
+                
+                //Add annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = self.restaurant.name
+                annotation.subtitle = self.restaurant.type
+                
+                if let location = placemark.location {
+                    //Display the annotation
+                    annotation.coordinate = location.coordinate
+                    
+                    self.mapView.showAnnotations([annotation], animated: true)
+                    self.mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+        })
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyMarker"
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            return nil
+        }
+        
+        var annotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        
+        annotationView?.glyphText = "😋"
+        annotationView?.markerTintColor = .orange
+        
+        return annotationView
+    }
+}
---
+++ FoodPin/View/RestaurantDetailSeparatorCell.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantDetailSeparatorCell.swift
+//  FoodPin
+//
+//  Created by Öcalan on 05/03/2019.
+//  Copyright © 2019 Froidefond Valentin. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailSeparatorCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
