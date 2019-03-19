bc54e8b05d5ec1f0bc5b16688bcc7a77169e9720
ValPacino <val.froidefond@gmail.com>
2019-03-19 17:57:41 +0100

    Getting Started with WKWebView and SFSafariViewController

---
+++ FoodPin/Controller/WebViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WebViewController.swift
+//  FoodPin
+//
+//  Created by Öcalan on 19/03/2019.
+//  Copyright © 2019 Froidefond Valentin. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet var webView: WKWebView!
+    
+    var targetURL = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let url = URL(string: targetURL) {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+
+}
